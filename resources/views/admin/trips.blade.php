3299a25909b62c5a25e4e30eabf304b3ebab7a2c
Derek Oliveira <derek.foliveira@gmail.com>
2026-06-16 19:30:30 -0300

    Rotas adicionadas
    Rotas das páginas de trips e users adicionada

--- resources/views/admin/trips.blade.php
+++ resources/views/admin/trips.blade.php
@@ -7,10 +7,17 @@
 <body class="bg-gray-50 font-sans antialiased flex">
 
     <aside class="w-64 bg-purple-900 text-white min-h-screen p-6 flex flex-col justify-between">
-        <div>
+         <div>
             <div class="flex items-center space-x-3 mb-10">
                 <img src="{{ asset('img/coinpellogobranco.png') }}" alt="COINPEL Logo" class="h-8">
             </div>
+            <nav class="space-y-4 text-sm font-medium opacity-80">
+                <a href="#" class="block hover:opacity-100">Clientes</a>
+                <a href="#" class="block hover:opacity-100">Motoristas</a>
+                <a href="#" class="block hover:opacity-100">Estatísticas</a>
+                <a href="#" class="block hover:opacity-100">Veículos</a>
+                <a href="#" class="block text-white opacity-100 font-bold bg-purple-800 -mx-6 px-6 py-2 border-l-4 border-l-orange-400">Viagens</a>
+            </nav>
         </div>
     </aside>
 
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/login', function () {
     return view('auth.login', ['step' => 'login']);
+});
+
+Route::get('/users', function () {
+    $mockUsers = [
+        (object)['name' => 'Maria Antônia da Silva', 'email' => 'M.Antonia@gmail.com'],
+        (object)['name' => 'Ricardo Martins', 'email' => 'R.Martins@gmail.com'],
+        (object)['name' => 'João Melo', 'email' => 'J.Melo@gmail.com'],
+        (object)['name' => 'Jones Radtke', 'email' => 'J.Radtke@gmail.com'],
+    ];
+    return view('admin.users', ['users' => $mockUsers]);
+});
+
+Route::get('/trips', function () {
+    $mockTrips = [
+        (object)['status' => 'Em andamento', 'name' => 'Prova FURG', 'date' => '30/03/2021', 'time' => '15:00', 'route' => 'Pelotas > Rio grande', 'vehicle' => '202 - Ônibus', 'rule' => 'Faculdade', 'driver' => 'Carlos'],
+        (object)['status' => 'Completa', 'name' => 'ChocoFest', 'date' => '30/03/2021', 'time' => '15:00', 'route' => 'Pelotas > Gramado', 'vehicle' => '202 - Ônibus', 'rule' => 'Turismo', 'driver' => 'Carlos'],
+        (object)['status' => 'Em andamento', 'name' => 'Beto Carrero', 'date' => '30/03/2021', 'time' => '15:00', 'route' => 'Pelotas > Balneário (...)', 'vehicle' => '202 - Ônibus', 'rule' => 'Turismo', 'driver' => 'Carlos'],
+    ];
+    return view('admin.trips', ['trips' => $mockTrips]);
 });
 
 Route::get('/', function () {
